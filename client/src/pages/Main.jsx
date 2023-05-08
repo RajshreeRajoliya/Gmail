6e2355f39bb0c52cfdc211e8360927bd2ebaf4e4
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-09 00:21:44 +0530

    Components

---
+++ client/src/pages/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../components/Header'
+import Sidebar from '../components/Sidebar'
+
+const Main = () => {
+  return (
+    <div>
+      <Header/>
+      <Sidebar/>
+    </div>
+  )
+}
+
+export default Main
