d5ae36c841e493f1cb0793d40ee1b270556d44fd
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-09 01:28:36 +0530

    sideBar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
-
+import { Drawer, styled } from "@mui/material";
 const Sidebar = () => {
+    const StyledDrawer = styled(Drawer)`
+    margin-top: 54px;
+`
   return (
-    <div>
-      Sidebar
-    </div>
+    <StyledDrawer
+    anchor='left'
+    open={true}
+    hideBackdrop={true}
+    ModalProps={{
+    keepMounted: true,
+    }}
+    variant="persistent"
+    sx={{
+        '& .MuiDrawer-paper': { 
+            width: 250,
+            borderRight: 'none',
+            background: '#f5F5F5',
+            marginTop: '64px',
+            height: 'calc(100vh - 64px)'
+        },
+    }}
+  >
+  hello 
+    {/* <SideBarContent /> */}
+   
+</StyledDrawer>
   )
 }
 
