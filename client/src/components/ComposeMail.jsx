9283faa39ebfe27997c8a77bbbf0afe9ee271a45
RajshreeRajoliya <rajshreerajoliya14@gmail.com>
2023-05-09 02:55:34 +0530

    CSSDialog

--- client/src/components/ComposeMail.jsx
+++ client/src/components/ComposeMail.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Dialog, styled, Typography, Box, InputBase, TextField, Button } from '@mui/material'; 
+import { Close, DeleteOutline } from '@mui/icons-material';
 
 const dialogStyle = {
     height: '90%',
@@ -9,6 +10,16 @@ const dialogStyle = {
     boxShadow: 'none',
     borderRadius: '10px 10px 0 0',
 }
+const Header = styled(Box)`
+    display: flex;
+    justify-content: space-between;
+    padding: 10px 15px;
+    background: #f2f6fc;
+    & > p {
+        font-size: 14px;
+        font-weight: 500;
+    }
+`
 
 const ComposeMail = () => {
   return (
@@ -16,7 +27,12 @@ const ComposeMail = () => {
      open={true}
      PaperProps={{ sx: dialogStyle }}
     >
-
+<Header><Typography>New Message</Typography>
+<Close fontSize="small" />
+</Header>
+<Box></Box>
+<Box></Box>
+<Box></Box>
     </Dialog>
   )
 }
